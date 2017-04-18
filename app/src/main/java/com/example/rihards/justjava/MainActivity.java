deb2866f244fc546f53a122c8c094ddb5bfde5e0
Maija <dislere.maija@gmail.com>
2017-04-18 23:47:07 +0300

    added checkbox

--- app/src/main/java/com/example/rihards/justjava/MainActivity.java
+++ app/src/main/java/com/example/rihards/justjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.rihards.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -37,8 +38,12 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the - button is clicked.
      */
     public void submitOrder(View view) {
+
+        CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.whippedCreamCheckbox);
+        boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
+
         int price = calculatePrice();
-        String priceMessage = createOrderSummary(price);
+        String priceMessage = createOrderSummary(price, hasWhippedCream);
         displayMessage(priceMessage);
     }
 
@@ -75,8 +80,9 @@ public class MainActivity extends AppCompatActivity {
      * @return the text summary
      */
 
-    private String createOrderSummary(int price) {
+    private String createOrderSummary(int price, boolean hasWhippedCream) {
         String priceMessage = "Name: Kaptain Kunal";
+        priceMessage += "\nAdd whipped cream?" + hasWhippedCream;
         priceMessage += "\nQuantity: " + quantity;
         priceMessage += "\nTotal: $" + price;
         priceMessage += "\nThank you!";
